be4db57bf9186f77bcd6991cf29bab962d47e1eb
Igor Chernov <cher.igor@outlook.com>
2021-05-26 17:01:16 +0300

    Добавил ToggleButton

--- HelloJuce/Source/MainComponent.h
+++ HelloJuce/Source/MainComponent.h
@@ -4,24 +4,22 @@
 
 //==============================================================================
 /*
-    This component lives inside our window, and this is where you should put all
-    your controls and content.
+	This component lives inside our window, and this is where you should put all
+	your controls and content.
 */
-class MainComponent  : public juce::Component
-{
-public:
-    //==============================================================================
-    MainComponent();
-    ~MainComponent() override;
+class MainComponent : public juce::Component {
+	public:
+	//==============================================================================
+	MainComponent();
+	~MainComponent() override;
 
-    //==============================================================================
-    void paint (juce::Graphics&) override;
-    void resized() override;
+	//==============================================================================
+	void paint(juce::Graphics&) override;
+	void resized() override;
 
-private:
-    //==============================================================================
-    // Your private member variables go here...
+	private:
+	juce::ToggleButton toggleButton;
 
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
